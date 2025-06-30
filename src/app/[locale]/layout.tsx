3398eead3b0bdae31b1c94e5f798051725c27c2b
Ömer Faruk Küçük <omerkucuk@teknasyon.com>
2025-06-30 15:50:18 +0300

    init: project base

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import { Geist } from 'next/font/google';
+import '../globals.css';
+import { hasLocale } from 'next-intl';
+import { routing } from '@/i18n/routing';
+import { notFound } from 'next/navigation';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Weather App',
+  description: 'Weather App built with Next.js and WeatherAPI',
+};
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}>) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+    <html lang={locale} className="h-full">
+      <body className={`${geistSans.variable} antialiased h-full`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-full bg-white grid grid-cols-[392px_1fr]">
+      <div className="bg-gradient-to-tr from-sky-900 to-sky-600"></div>
+      <div className="h-full"></div>
+    </div>
+  );
+}
