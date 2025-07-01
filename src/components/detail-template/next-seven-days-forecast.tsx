52a99d05022f9b2eb58c4fa52274947d497ff9af
Ömer Faruk Küçük <omerkucuk@teknasyon.com>
2025-07-02 02:33:17 +0300

    fix: component typo fix - 5 to 7 days

--- src/components/detail-template/next-seven-days-forecast.tsx
+++ src/components/detail-template/next-seven-days-forecast.tsx
@@ -1,34 +1,35 @@
 import { Calendar1Icon } from 'lucide-react';
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import dayjs from '@/lib/dayjs';
 import { useWeatherStore } from '@/providers/weather-store-provider';
 import { WeatherResponse } from '@/types/weather';
 import { WeatherCard } from '../weather-card';
 
-export interface Next5DaysForecastProps {
+export interface Next7DaysForecastProps {
   data: WeatherResponse;
 }
 
-export function Next5DaysForecast({ data }: Next5DaysForecastProps) {
+export function Next7DaysForecast({ data }: Next7DaysForecastProps) {
   const t = useTranslations('detail-template');
+  const locale = useLocale();
   const weatherStore = useWeatherStore((state) => state);
 
   return (
-    <>
-      <h2 className="text-2xl font-semibold mt-8 mb-4">{t('detail-template.next-five-days')}</h2>
-      <div className="grid grid-cols-4 gap-4 items-stretch">
+    <div className="pb-8">
+      <h2 className="text-xl lg:text-2xl font-semibold mt-8 mb-4">{t('detail-template.next-seven-days')}</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 items-stretch">
         {data.forecast.forecastday.map((day) => (
           <WeatherCard
             key={day.date_epoch}
             isDay={1}
             icon={<Calendar1Icon size={18} />}
             conditionCode={day.day.condition.code}
-            name={dayjs(day.date).format('dddd')}
+            name={dayjs(day.date).locale(locale).format('dddd')}
             temp={weatherStore.selectedUnit === 'c' ? day.day.avgtemp_c : day.day.avgtemp_f}
             description={day.day.condition.text}
           />
         ))}
       </div>
-    </>
+    </div>
   );
 }
